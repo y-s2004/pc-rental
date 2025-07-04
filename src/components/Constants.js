1e7d4a9d6d6066b87e6ae32114165e3ad7028e4d
Y.S <saitou.yoshiki.8@gmail.com>
2025-07-04 14:13:54 +0900

    SUmmaryaaaaa

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -17,15 +17,15 @@ export const emptyUser = {
 };
 
 export const setNewDevice = {
-            asset_num: '',
-            maker: '',
-            os: '',
-            memory: '',
-            graphic_board: '',
-            place: '',
-            rental_start: '',
-            rental_deadline: '',
-            inventory_date: '',
-            disc_capacity: '',
-            remarks: '',
+    asset_num: '',
+    maker: '',
+    os: '',
+    memory: '',
+    graphic_board: '',
+    place: '',
+    rental_start: '',
+    rental_deadline: '',
+    inventory_date: '',
+    disc_capacity: '',
+    remarks: '',
 };
