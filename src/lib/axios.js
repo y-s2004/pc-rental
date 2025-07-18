27286533a1ade1316d712f350655c971639436a2
Y.S <saitou.yoshiki.8@gmail.com>
2025-07-18 16:49:26 +0900

    SummarySummary

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+const baseUrls = [
+        "http://localhost:8080",
+        "http://backend:8080",
+];
+
+const selectedBaseurl = process.env.NEXT_PUBLIC_API_BASE || baseUrls[0];
+
 export const axiosInstance = axios.create({
-    baseURL: "http://backend:8080",
+    baseURL: selectedBaseurl,
     headers: {
         'Content-Type': 'application/json',
     },
